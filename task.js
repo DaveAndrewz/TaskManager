dff9ac455ead0e63129f4b47148fa370c5049bd7
Dave Andrewz <DaveAndrewz@hotmail.com>
2021-10-19 14:17:54 -0500

    The removing a task feature is complete

--- task.js
+++ task.js
@@ -26,6 +26,11 @@ let showTasks = ``;
 // For storing the value of a new task
 let newTask;
 
+// For storing the number of the task to remove
+let num;
+
+// For storing the value of the removed task
+let removed;
 
 
 
@@ -64,14 +69,39 @@ while (userInput !== `CLOSE`){
         // Alerts the user that their new task has been added
         alert(`"${newTask}" has been added!`);
 
+        // Adds the users entry as a new item at the end of the tasks array
+        tasks.push(newTask);
     }
 
+    // Checks if user entered REMOVE
+    if (userInput === `REMOVE`){
 
-    
+        // Concatenates each task/item in the tasks array to the showTasks string variable. Also, sets/displays a number for each task
+        for (i = 0; i < tasks.length; i++){
+
+            // Adding 1 to i (AKA the index) so the numbers will start displaying at 1. Also, using "\n" to create a new line (AKA Line break)
+            showTasks += `${i + 1}: ${tasks[i]}\n`;
+        }
+        
+        // Prompts the user to enter a number and stores their response to the "num" variable.
+        // Also, using "\n" to create a new line (AKA Line break)
+        // Subtracting 1 from the users entry so that it matches the index of the item that the user wants to be removed from the tasks array.
+        // Just like with ParseInt, JavaScript will attempt to convert a string into a number when you try to subtract from it. So ParseInt is NOT necessary here.
+        num = prompt(`Please enter a number to remove:\n\n${showTasks}`) - 1;
+
+        // Removes the task/item selected by the user from the tasks array. Also, sets the task/item that was removed to the REMOVED variable (NOTE: Splice returns the value that is removed as an item(s) in an array)
+        removed = tasks.splice(num, 1);
+
+        // Alerts user with the task/item that has been removed (NOTE: Using index on the REMOVED variable here because splice returns the value that is removed as an item in an array)
+        alert(`"${removed[0]}" has been removed!`);
+
+        // Sets the value of the showTasks string variable back to an empty string
+        showTasks = ``;
+    }
     // Displays the menu again
     userInput = prompt(menu);
 }
 
 
 // Alerts the user that they have closed the program
-alert(`Thanks for using the To-Do List again dude!!`)
+alert(`Thanks for using the To-Do List dude!!`)
